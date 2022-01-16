2ca6a17099079ab89f6e2708cc1e242916636719
eurico77 <euricomagalhaes61@gmail.com>
2022-01-16 16:55:25 -0300

    creating a example array

---
+++ arrays/index.js
@@ -0,0 +1,14 @@
+const valores = [7.7, 8.9, 6.3, 9.2]
+console.log(valores[0], valores[3])
+console.log(valores[4])
+
+valores[4] = 10
+console.log(valores)
+
+valores.push({ id: 3 }, null, false, 'teste')
+console.log(valores)
+
+
+
+valores.values()
+
